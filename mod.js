c2d06d58d5a33850c80698c655907537d6e93236
Ian Fabs <ian@fabs.dev>
2020-03-09 17:29:14 -0400

    Final updates

--- mod.js
+++ mod.js
@@ -3,23 +3,22 @@ const { args, run } = Deno;
 console.log(args);
 
 const [slug, dest] = args.filter(f => f.indexOf("-") !== 0);
-const flags = args.filter(f => f.indexOf("-") === 0);
+const flags = args.filter(f => f.indexOf("-") === 0).map(f => f.toLowerCase());
 
 console.log(slug, dest, flags);
 
 const clone = async (slug, dest, flags) => {
-  let host;
-  if (flags.map(f => f.toLowerCase()).includes('-lab')) {
-    host = "lab";
-  } else {
-    host = "hub"
-  }
-  const url = `https://git${host}.com/${slug}`;
+  let host = "hub";
 
-  let gitProcessArgs = ["git","clone", url];
+  const url = `https://git${host}.com/${slug}`;
+  let gitArgs = ["git","clone", url];
+
+  if (dest != undefined && dest != null) gitArgs.push(dest);
+  if ( flags.includes('-lab') ) host = "lab";
+  if ( flags.includes('-f') ) gitArgs.push("--force");
+
   let process = run({
-    // args: Object.values( { ...gitProcessArgs, ...dest && { dest } } )
-    args: dest != undefined ? [ ...gitProcessArgs, dest ] : gitProcessArgs,
+    args: dest != undefined ? [ ...gitArgs, dest ] : gitArgs,
   });
 
   let result = await process.status();
